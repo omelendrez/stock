045f68902e662a244957e467d0170d55c173d2be
omelendrez <omar.melendrez@gmail.com>
2019-10-16 20:59:38 -0300

    Reemplazar Categories por Companiesç

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css';
 import ToolBar from './components/ToolBar';
 import Home from './components/Home'
-import Categories from './components/Categories'
+import Companies from './components/Companies'
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
       <Router>
         <ToolBar />
         <Route exact path="/" component={Home} />
-        <Route path="/categories" component={Categories} />
+        <Route path="/companies" component={Companies} />
       </Router>
     </div>
   );
