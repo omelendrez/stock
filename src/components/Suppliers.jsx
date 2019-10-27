44b55c2cb4e7cffafd0c9d4dc53575624ba4b724
Emarengo73 <emarengo73@gmail.com>
2019-10-26 22:23:32 -0300

    Habilitar y guardar datos al formulario

--- src/components/Suppliers.jsx
+++ src/components/Suppliers.jsx
@@ -1,15 +1,26 @@
 import React, { useState, useEffect } from 'react'
-import { getSuppliers } from './../services/suppliers'
+import { getSuppliers, saveSupplier } from './../services/suppliers'
 import { getStatus } from './../services/status'
 import { getCompanies } from './../services/companies'
 import Table from './common/Table'
 import Form from './common/Form'
 
 const Suppliers = () => {
+  const defaultSupplier = {
+    id: -1,
+    code: "",
+    name: "",
+    phoneNumber: "",
+    address: "",
+    contact: "",
+    companyId: "",
+    statusId: "1",
+  }
   const [suppliers, setSuppliers] = useState([])
   const [showForm, setShowForm] = useState(false)
   const [status, setStatus] = useState([])
   const [companies, setCompanies] = useState([])
+  const [supplier, setSupplier] = useState(defaultSupplier)
 
   useEffect(() => {
     const suppliers = getSuppliers()
@@ -27,13 +38,29 @@ const Suppliers = () => {
 
   const save = e => {
     e.preventDefault()
+    saveSupplier(supplier)
+    setSupplier(defaultSupplier)
     setShowForm(false)
   }
 
   const cancel = e => {
     e.preventDefault()
+    setSupplier(defaultSupplier)
     setShowForm(false)
   }
+
+  const updateForm = e => {
+    e.preventDefault()
+    const newSupplier = { ...supplier, [e.target.id]: e.target.value }
+    setSupplier(newSupplier)
+  }
+
+  const editRecord = supplier => {
+    setSupplier(supplier)
+    setShowForm(true)
+  }
+
+  const { code, name, phoneNumber, address, contact, companyId, statusId } = supplier
 
   return (
     <React.Fragment>
@@ -41,47 +68,48 @@ const Suppliers = () => {
         {suppliers.length && <Table
           title="Suppliers"
           records={suppliers}
+          editRecord={editRecord}
         />}
-        <button className="btn btn-primary m-2" onClick={e => addRecord(e)}>Add Suppliers</button>
+        <button className="btn btn-primary m-2" onClick={e => addRecord(e)}>Add Supplier</button>
       </React.Fragment>}
       {showForm &&
         <Form title="Suppliers" save={save} cancel={cancel}>
 
           <div className="form-group">
             <label htmlFor="code">Code</label>
-            <input type="text" id="code" className="form-control" />
+            <input type="text" id="code" className="form-control" value={code} onChange={e => updateForm(e)} />
           </div>
 
           <div className="form-group">
             <label htmlFor="name">Name</label>
-            <input type="text" id="name" className="form-control" />
+            <input type="text" id="name" className="form-control" value={name} onChange={e => updateForm(e)} />
           </div>
 
           <div className="form-group">
             <label htmlFor="phoneNumber">Phone Number</label>
-            <input type="text" id="phoneNumber" className="form-control" />
+            <input type="text" id="phoneNumber" className="form-control" value={phoneNumber} onChange={e => updateForm(e)} />
           </div>
 
           <div className="form-group">
             <label htmlFor="address">Address</label>
-            <input type="text" id="address" className="form-control" />
+            <input type="text" id="address" className="form-control" value={address} onChange={e => updateForm(e)} />
           </div>
 
           <div className="form-group">
             <label htmlFor="contact">Contact</label>
-            <input type="text" id="contact" className="form-control" />
+            <input type="text" id="contact" className="form-control" value={contact} onChange={e => updateForm(e)} />
           </div>
 
           <div class="form-group">
             <label htmlFor="companyId">Company</label>
-            <select className="form-control" id="companyId">
+            <select className="form-control" id="companyId" value={companyId} onChange={e => updateForm(e)} >
               {companies.map(st => <option value={st.id}>{st.name}</option>)}
             </select>
           </div>
 
           <div class="form-group">
             <label htmlFor="statusId">Status</label>
-            <select className="form-control" id="statusId">
+            <select className="form-control" id="statusId" value={statusId} onChange={e => updateForm(e)} >
               {status.map(st => <option value={st.id}>{st.name}</option>)}
             </select>
           </div>
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { getUsers } from './../services/users'
+import { getUsers, saveUser } from './../services/users'
 import { getStatus } from './../services/status'
 import { getCompanies } from './../services/companies'
 import { getProfiles } from './../services/profiles'
@@ -7,11 +7,21 @@ import Table from './common/Table'
 import Form from './common/Form'
 
 const Users = () => {
+  const defaultUser = {
+    id: -1,
+    userName: "",
+    email: "",
+    fullName: "",
+    profileId: "",
+    companyId: "",
+    statusId: "",
+  }
   const [users, setUsers] = useState([])
   const [showForm, setShowForm] = useState(false)
   const [status, setStatus] = useState([])
   const [companies, setCompanies] = useState([])
   const [profiles, setProfiles] = useState([])
+  const [user, setUser] = useState(defaultUser)
 
   useEffect(() => {
     const users = getUsers()
@@ -31,13 +41,29 @@ const Users = () => {
 
   const save = e => {
     e.preventDefault()
+    saveUser(user)
+    setUser(defaultUser)
     setShowForm(false)
   }
 
   const cancel = e => {
     e.preventDefault()
+    setUser(defaultUser)
     setShowForm(false)
   }
+
+  const updateForm = e => {
+    e.preventDefault()
+    const newUser = { ...user, [e.target.id]: e.target.value }
+    setUser(newUser)
+  }
+
+  const editRecord = user => {
+    setUser(user)
+    setShowForm(true)
+  }
+
+  const { userName, email, fullName, profileId, companyId, statusId } = user
 
   return (
     <React.Fragment>
@@ -45,45 +71,45 @@ const Users = () => {
         {users.length && <Table
           title="Users"
           records={users}
+          editRecord={editRecord}
         />}
-
-        <button className="btn btn-primary m-2" onClick={e => addRecord(e)}>Add Users</button>
+        <button className="btn btn-primary m-2" onClick={e => addRecord(e)}>Add User</button>
       </React.Fragment>}
       {showForm &&
         <Form title="Users" save={save} cancel={cancel}>
 
           <div className="form-group">
             <label htmlFor="userName">User Name</label>
-            <input type="text" id="userName" className="form-control" />
+            <input type="text" id="userName" className="form-control" value={userName} onChange={e => updateForm(e)} />
           </div>
 
           <div className="form-group">
             <label htmlFor="email">Email</label>
-            <input type="text" id="email" className="form-control" />
+            <input type="text" id="email" className="form-control" value={email} onChange={e => updateForm(e)} />
           </div>
 
           <div className="form-group">
             <label htmlFor="fullName">Full Name</label>
-            <input type="text" id="fullName" className="form-control" />
+            <input type="text" id="fullName" className="form-control" value={fullName} onChange={e => updateForm(e)} />
           </div>
 
           <div class="form-group">
             <label htmlFor="profileId">Profiles</label>
-            <select className="form-control" id="profileId">
+            <select className="form-control" id="profileId" value={profileId} onChange={e => updateForm(e)} >
               {profiles.map(st => <option value={st.id}>{st.name}</option>)}
             </select>
           </div>
 
           <div class="form-group">
             <label htmlFor="companyId">Companies</label>
-            <select className="form-control" id="companyId">
+            <select className="form-control" id="companyId" value={companyId} onChange={e => updateForm(e)} >
               {companies.map(st => <option value={st.id}>{st.name}</option>)}
             </select>
           </div>
 
           <div class="form-group">
             <label htmlFor="statusId">Status</label>
-            <select className="form-control" id="statusId">
+            <select className="form-control" id="statusId" value={statusId} onChange={e => updateForm(e)} >
               {status.map(st => <option value={st.id}>{st.name}</option>)}
             </select>
           </div>
