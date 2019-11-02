da213297b4ea496ea7f33a714cdb2e5081b43136
Emarengo73 <emarengo73@gmail.com>
2019-11-02 14:26:45 -0300

    feature_31

--- src/services/users.js
+++ src/services/users.js
@@ -1,25 +1,27 @@
-const users = [
-  {
-    id: 1,
-    userName: 'Omar',
-    email: 'omar.melendrez@gmail.com',
-    fullName: 'Omar Melendrez',
-    profileId: 'Administrator',
-    companyId: 'Veterinaria Colitas Felices',
-    statusId: 'Active'
-  },
-  {
-    id: 2,
-    userName: 'Angie',
-    email: 'angeles.melendrez@gmail.com',
-    fullName: 'Ma de los Angeles Melendrez',
-    profileId: 'Administrator',
-    companyId: 'Veterinaria Colitas Felices',
-    statusId: 'Active'
-  }
+import http from './api'
 
-]
+export const getUsers = async () => {
+  const response = await http.get('/users')
+  return response.data.users
+}
 
-export const getUsers = () => users
-export const saveUser = user => console.log(user)
-export const deleteUser = user => console.log(user)
+export const saveUser = user => {
+  return new Promise((resolve, reject) => {
+    http.post('/users', user)
+      .then(response => {
+        resolve(response.data)
+      })
+      .catch(error => reject(error))
+  })
+}
+
+export const deleteUser = user => {
+  return new Promise((resolve, reject) => {
+    const { id } = user
+    http.delete(`/users/${id}`)
+      .then(response => {
+        resolve(response.data)
+      })
+      .catch(error => reject(error))
+  })
+}
