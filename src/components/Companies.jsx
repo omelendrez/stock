8fad05a208ef23926313742a1ad8a9a351b6bdf3
omelendrez <omar.melendrez@gmail.com>
2019-11-03 15:19:24 -0300

    Implementar feedback de backend

--- src/components/Companies.jsx
+++ src/components/Companies.jsx
@@ -3,6 +3,8 @@ import { getCompanies, saveCompany, deleteCompany } from './../services/companie
 import { getStatus } from './../services/status'
 import Table from './common/Table'
 import Form from './common/Form'
+// TODO
+import Alert from './common/Alert'
 
 const Companies = () => {
   const defaultCompany = {
@@ -15,13 +17,20 @@ const Companies = () => {
   const [showForm, setShowForm] = useState(false)
   const [status, setStatus] = useState([])
   const [company, setCompany] = useState(defaultCompany)
+  const [response, setResponse] = useState({})
 
-  // TODO
   useEffect(() => {
     fetchData()
   }, [])
 
   // TODO
+  useEffect(() => {
+    const delay = response.success ? 3000 : 10000
+    setTimeout(() => {
+      setResponse({})
+    }, delay)
+  }, [response])
+
   const fetchData = async () => {
     const status = await getStatus()
     setStatus(status)
@@ -36,11 +45,15 @@ const Companies = () => {
   }
 
   // TODO
-  const save = async e => {
+  const save = e => {
     e.preventDefault()
-    await saveCompany(company)
-    fetchData()
-    setShowForm(false)
+    saveCompany(company)
+      .then(data => {
+        setResponse(data)
+        fetchData()
+        setShowForm(false)
+      })
+      .catch(err => setResponse(err.response.data))
   }
 
   const cancel = e => {
@@ -60,9 +73,13 @@ const Companies = () => {
   }
 
   // TODO
-  const deleteRecord = async company => {
-    await deleteCompany(company)
-    fetchData()
+  const deleteRecord = company => {
+    deleteCompany(company)
+      .then(data => {
+        setResponse(data)
+        fetchData()
+      })
+      .catch(err => setResponse(err.response.data))
   }
 
   const { code, name, statusId } = company
@@ -76,6 +93,8 @@ const Companies = () => {
           editRecord={editRecord}
           deleteRecord={deleteRecord}
         />
+        <Alert response={response} />
+
         <button className="btn btn-primary m-2" onClick={e => addRecord(e)}>Add Company</button>
       </React.Fragment>}
       {showForm &&
@@ -98,8 +117,11 @@ const Companies = () => {
             </select>
           </div>
 
+          <Alert response={response} />
+
         </Form>
       }
+      {/*TODO*/}
     </React.Fragment>
   )
 }
---
+++ src/components/common/Alert.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Alert = ({ response }) => {
+  const { message, success, error } = response
+  const style = `alert alert-${success ? 'success' : 'danger'} alert-dismissible fade show`
+  const responseMessage = success ? message : error
+  return (
+    responseMessage ?
+      (<div className={style} role="alert">
+        {responseMessage}
+        <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button></div>)
+      :
+      null
+  )
+}
+
+export default Alert
